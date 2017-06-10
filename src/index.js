b2078e7cf3b52fd2059abdc1df431cf7c426e436
Wilde <gutopicciani@yahoo.com.br>
2017-06-09 21:52:32 -0300

    Removendo as viadage gerada, e colocando o bootstrap no projeto

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './App/App';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+	<App />, 
+	document.getElementById('root')
+);

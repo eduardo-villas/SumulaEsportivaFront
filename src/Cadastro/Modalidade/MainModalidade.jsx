98bd7c7ee84488a09990c41b0ac8db093a2c5b33
Wilde <gutopicciani@yahoo.com.br>
2017-07-23 03:31:08 -0300

    Listagem de modalidade

--- src/Cadastro/Modalidade/MainModalidade.jsx
+++ src/Cadastro/Modalidade/MainModalidade.jsx
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import ListagemModalidade from './ListagemModalidade'
 
 class MainModalidade extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			tela: 'listagem'
+		}
+	}
+
+	onTelaChange = (tela, id) => {
+		this.setState({
+			tela: tela,
+			id: id
+		});
+	}
+
 	render() {
+		let tela = <ListagemModalidade onTelaChange={this.onTelaChange}/>;
 		return (
 			<div>
-				<h1>Listagem de Modalidade virá aqui.</h1>
+				<h3>Cadastro de modalidade</h3>
+				{tela}
 			</div>
 		);
 	}

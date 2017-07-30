27e88ee047da7f1ce0ad567015fa3af050996a7c
Wilde <gutopicciani@yahoo.com.br>
2017-07-29 23:59:28 -0300

    Manutenção no formulario de Modalidade, inserindo tipos de pontuacao e punicao

--- src/Cadastro/Modalidade/FormularioModalidade.jsx
+++ src/Cadastro/Modalidade/FormularioModalidade.jsx
@@ -5,6 +5,7 @@ import {
 	FormControl,
 	Button
 } from 'react-bootstrap';
+import FontAwesome from 'react-fontawesome';
 import Service from './ModalidadeService.jsx';
 
 class FormularioModalidade extends Component {
@@ -17,7 +18,13 @@ class FormularioModalidade extends Component {
 			quantidadeTemposExtras: '',
 			duracaoExtra: '',
 			quantidadeTimeout: '',
-			duracaoTimeout: ''
+			duracaoTimeout: '',
+			tiposPunicao: [],
+			tiposPonto: [],
+			descricaoPonto: '',
+			valorPonto: '',
+			tipoPunicao: '',
+			descricaoPunicao: ''
 		};
 		if (props.id) {
 			let service = new Service();
@@ -34,7 +41,9 @@ class FormularioModalidade extends Component {
 			quantidadeTemposExtras: entity.tempo.quantidadeTemposExtras,
 			duracaoExtra: entity.tempo.duracaoExtra,
 			quantidadeTimeout: entity.tempo.quantidadeTimeout,
-			duracaoTimeout: entity.tempo.duracaoTimeout
+			duracaoTimeout: entity.tempo.duracaoTimeout,
+			tiposPonto: entity.tiposPonto,
+			tiposPunicao: entity.tiposPunicao
 		});
 	};
 
@@ -49,7 +58,9 @@ class FormularioModalidade extends Component {
 				duracaoExtra: this.state.duracaoExtra,
 				quantidadeTimeout: this.state.quantidadeTimeout,
 				duracaoTimeout: this.state.duracaoTimeout
-			}
+			},
+			tiposPonto: this.state.tiposPonto,
+			tiposPunicao: this.state.tiposPunicao
 		}
 		if (this.state.id) {
 			entity.id = this.state.id;
@@ -60,7 +71,74 @@ class FormularioModalidade extends Component {
 		});
 	};
 
+	adicionarPonto = () => {
+		let pontos = this.state.tiposPonto;
+		pontos.push({
+			descricao: this.state.descricaoPonto,
+			valor: this.state.valorPonto
+		});
+		this.setState({
+			tiposPonto: pontos,
+			descricaoPonto: '',
+			valorPonto: ''
+		});
+	};
+
+	removerPonto = (tipoPonto) => {
+		let pontos = this.state.tiposPonto;
+		let index = pontos.indexOf(tipoPonto);
+		pontos.splice(index, 1);
+		this.setState({ tiposPonto: pontos });
+	};
+
+	adicionarPunicao = () => {
+		let punicoes = this.state.tiposPunicao;
+		punicoes.push({
+			tipo: this.state.tipoPunicao,
+			descricao: this.state.descricaoPunicao
+		});
+		this.setState({
+			tiposPunicao: punicoes,
+			tipoPunicao: '',
+			descricaoPunicao: ''
+		});
+	};
+
+	removerPunicao = (tipoPunicao) => {
+		let punicoes = this.state.tiposPunicao;
+		let index = punicoes.indexOf(tipoPunicao);
+		punicoes.splice(index, 1);
+		this.setState({ tiposPunicao: punicoes });
+	};
+
 	render() {
+		let key = 1;
+		let pontos = this.state.tiposPonto.map((tipoPonto) => {
+			return (
+				<tr key={key++} className="background-white">
+					<td>{tipoPonto.descricao}</td>
+					<td>{tipoPonto.valor}</td>
+					<td className="centralized">
+						<Button bsStyle="danger" onClick={() => this.removerPonto(tipoPonto)}>
+							<FontAwesome name="times"/>
+						</Button>
+					</td>
+				</tr>
+			);
+		});
+		let punicoes = this.state.tiposPunicao.map((tipoPunicao) => {
+			return (
+				<tr key={key++} className="background-white">
+					<td>{tipoPunicao.tipo}</td>
+					<td>{tipoPunicao.descricao}</td>
+					<td className="centralized">
+						<Button bsStyle="danger" onClick={() => this.removerPunicao(tipoPunicao)}>
+							<FontAwesome name="times"/>
+						</Button>
+					</td>
+				</tr>
+			);
+		});
 		return (
 			<form>
 				<div className="row">
@@ -153,6 +231,97 @@ class FormularioModalidade extends Component {
 							</div>
 						</div>
 					</fieldset>
+				</div>
+				<h4>Tipos de ponto</h4>
+				<div className="row">
+					<div className="col-xs-12 col-sm-6">
+						<FormGroup>
+							<ControlLabel>Nome do tipo de ponto</ControlLabel>
+							<FormControl
+								value={this.state.descricaoPonto}
+								onChange={(descricaoPonto) => {this.setState({ descricaoPonto: descricaoPonto.target.value });}}
+							/>
+						</FormGroup>
+					</div>
+					<div className="col-xs-12 col-sm-4">
+						<FormGroup>
+							<ControlLabel>Valor do ponto</ControlLabel>
+							<FormControl
+								type="number"
+								value={this.state.valorPonto}
+								onChange={(valorPonto) => {this.setState({ valorPonto: valorPonto.target.value });}}
+							/>
+						</FormGroup>
+					</div>
+					<div className="col-xs-12 col-sm-2">
+						<FormGroup>
+							<ControlLabel className="invisible">.</ControlLabel>
+							<Button bsStyle="primary" className="btn-block" onClick={this.adicionarPonto}>Adicionar</Button>
+						</FormGroup>
+					</div>
+				</div>
+				<hr/>
+				<div className="row">
+					<div className="col-xs-12">
+						<ControlLabel>Pontos cadastrados</ControlLabel>
+						<table className="table table-striped table-bordered">
+							<thead>
+								<tr>
+									<th className="col-xs-6">Nome</th>
+									<th className="col-xs-5">Valor</th>
+									<th className="col-xs-1 centralized">Ações</th>
+								</tr>
+							</thead>
+							<tbody>
+								{pontos}
+							</tbody>
+						</table>
+					</div>
+				</div>
+				<h4>Tipos de punição</h4>
+				<div className="row">
+					<div className="col-xs-12 col-sm-4">
+						<FormGroup>
+							<ControlLabel>Nome do tipo de punição</ControlLabel>
+							<FormControl
+								value={this.state.tipoPunicao}
+								onChange={(tipoPunicao) => {this.setState({ tipoPunicao: tipoPunicao.target.value });}}
+							/>
+						</FormGroup>
+					</div>
+					<div className="col-xs-12 col-sm-6">
+						<FormGroup>
+							<ControlLabel>Descrição do tipo de punição</ControlLabel>
+							<FormControl
+								value={this.state.descricaoPunicao}
+								onChange={(descricaoPunicao) => {this.setState({ descricaoPunicao: descricaoPunicao.target.value });}}
+							/>
+						</FormGroup>
+					</div>
+					<div className="col-xs-12 col-sm-2">
+						<FormGroup>
+							<ControlLabel className="invisible">.</ControlLabel>
+							<Button bsStyle="primary" className="btn-block" onClick={this.adicionarPunicao}>Adicionar</Button>
+						</FormGroup>
+					</div>
+				</div>
+				<hr/>
+				<div className="row">
+					<div className="col-xs-12">
+						<ControlLabel>Punições cadastradas</ControlLabel>
+						<table className="table table-striped table-bordered">
+							<thead>
+								<tr>
+									<th className="col-xs-6">Nome</th>
+									<th className="col-xs-5">Descrição</th>
+									<th className="col-xs-1 centralized">Ações</th>
+								</tr>
+							</thead>
+							<tbody>
+								{punicoes}
+							</tbody>
+						</table>
+					</div>
 				</div>
 				<div className="row">
 					<div className="col-xs-12">
